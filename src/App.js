f49f203cb522bcbc90cae94efdffeafc3356d8cb
yongdilun <dilunyong@gmail.com>
2024-11-15 19:33:24 +0800

    redirect2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from "./components/Login";
 import Home from "./components/Home";
